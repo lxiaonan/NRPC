d6de98d96fb92696bb69c57ccf16bd79fc05f18a
xiaoNan <hello@xiaoNan.cn>
2024-02-18 12:49:04 +0800

    初始化RPC项目

---
+++ nrpc-interface/src/main/java/com/nrpc/interfaces/HelloService.java
@@ -0,0 +1,5 @@
+package com.nrpc.interfaces;
+
+public interface HelloService {
+    String sayHello(String msg);
+}
---
+++ nrpc-interface/src/main/java/com/nrpc/interfaces/LuckyIndexService.java
@@ -0,0 +1,9 @@
+package com.nrpc.interfaces;
+
+public interface LuckyIndexService {
+    /**
+     *  获取幸运指数
+     * @return 幸运值 1~6
+     */
+    public int getLuckyIndex();
+}
